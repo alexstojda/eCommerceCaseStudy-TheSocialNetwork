8d7177a2df707a683bdb66419477569f0787077a
SkiRacerDude <thereff10@hotmail.com>
2015-11-20 21:19:24 -0500

    Conflict resolved on \models

--- includes/models/inbox.php
+++ includes/models/inbox.php
@@ -27,7 +27,7 @@ class _Inbox extends model
                     FROM `messages`
                     LEFT JOIN `users` u1 ON u1.`user_id` = `messages`.`from_user_id`
                     LEFT JOIN `users` u2 ON u2.`user_id` = `messages`.`to_user_id`
-                   WHERE `to_user_id` = 2 AND `from_user_id` = 24
+                   WHERE `to_user_id` = :ida AND `from_user_id` = :idb
                   UNION
                   SELECT CONCAT(u1.`first_name`, ' ', u1.`last_name`) AS from_user,
                          from_user_id,
@@ -37,8 +37,8 @@ class _Inbox extends model
                     FROM `messages`
                     LEFT JOIN `users` u1 ON u1.`user_id` = `messages`.`from_user_id`
                     LEFT JOIN `users` u2 ON u2.`user_id` = `messages`.`to_user_id`
-                   WHERE `to_user_id` = 24 AND `from_user_id` = 2
-                   ORDER BY `timesent` DESC";
+                   WHERE `to_user_id` = :idb AND `from_user_id` = :ida
+                   ORDER BY `timesent` ASC";
         return $this->db->select($query,
             array(':ida' => $IDa, ':idb' => $IDb));
     }
@@ -79,5 +79,10 @@ class _Inbox extends model
     public function getName($uid)
     {
         return $this->db->select("SELECT first_name, last_name FROM users WHERE user_id = :id", array(':id' => $uid));
+    }
+
+    public function newMessage($from, $to, $message)
+    {
+        return $this->db->insert('messages', array('from_user_id' => $from, 'to_user_id' => $to, 'message' => $message));
     }
 }
